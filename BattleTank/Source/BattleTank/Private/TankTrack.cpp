c25da596fd07d02c5e1fb43b647486fbc78fc100
Kendy <k_jeune@live.concordia.ca>
2018-11-10 01:04:34 -0500

    Adding control and Track system

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle at : %f !!"), *Name, Throttle);
+
+	//calculate the force and apply to the center of the tracks and tank body
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+	
+}
+
