b02f9f25fda8de159e0e89fe94975b6d2001ac4e
Kendy <k_jeune@live.concordia.ca>
2018-12-04 21:32:10 -0500

    Implementing Spring to Tank

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -19,7 +19,7 @@ void ATankAIController::Tick(float DeltaTime)
 	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
 	auto ControlledTank = GetPawn();
 
-	if (!ensure(PlayerTank && ControlledTank)) { return; }
+	if (!(PlayerTank && ControlledTank)) { return; }
 
 	//Move towards the player
 	MoveToActor(PlayerTank, AcceptanceRadius);
